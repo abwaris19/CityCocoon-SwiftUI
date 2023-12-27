dc0fb078f9bbf7edfe42f50fe701ebf491e08085
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-28 00:49:57 +0100

    ExploreView

---
+++ CityCocoon/Core/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  CityCocoon
+//
+//  Created by Khan on 28.12.2023.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
---
+++ CityCocoon/Core/Listings/ListingItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingItemView.swift
+//  CityCocoon
+//
+//  Created by Khan on 28.12.2023.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
