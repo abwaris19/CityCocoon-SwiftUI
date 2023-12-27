a07fb640746a88a6c9b8e89167fc4616238c9d94
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-27 23:24:23 +0100

    Initial Commit

---
+++ CityCocoon/CityCocoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityCocoonApp.swift
+//  CityCocoon
+//
+//  Created by Khan on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CityCocoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
