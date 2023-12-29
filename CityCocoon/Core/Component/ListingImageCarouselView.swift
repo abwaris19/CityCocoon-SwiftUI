7232cb056663cb0bcd832a3fe5149f29cb22716f
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-29 23:17:45 +0100

    Listing Detial Continue ...

---
+++ CityCocoon/Core/Component/ListingImageCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingImageCarouselView.swift
+//  CityCocoon
+//
+//  Created by Khan on 29.12.2023.
+//
+
+import SwiftUI
+
+struct ListingImageCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingImageCarouselView()
+}
---
+++ CityCocoon/Core/Listings/ListingDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingDetailView.swift
+//  CityCocoon
+//
+//  Created by Khan on 29.12.2023.
+//
+
+import SwiftUI
+
+struct ListingDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingDetailView()
+}
