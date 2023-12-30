224d77daddb3c44db171445b764489dc2f6d0f74
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-30 23:29:10 +0100

    Destination SearchView

---
+++ CityCocoon/Core/Explore/DestinationSearch.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationSearch.swift
+//  CityCocoon
+//
+//  Created by Khan on 30.12.2023.
+//
+
+import SwiftUI
+
+struct DestinationSearch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationSearch()
+}
