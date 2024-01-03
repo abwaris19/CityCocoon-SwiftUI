ea87a59bd75b3e2008a378b081088a35e66d9afe
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-03 05:55:19 +0100

    UI Wrap Up

--- CityCocoon/Core/Wishlists/WishlistsView.swift
+++ CityCocoon/Core/Wishlists/WishlistsView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct WishlistsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationStack {
+            
+        VStack (alignment: .leading, spacing:32) {
+            
+            VStack (alignment: .leading, spacing: 4){
+                Text("Log in to view your wishlists")
+                    .font(.headline)
+                Text("You can create, view or edit whistslist once you've logged in ")
+                    .font(.footnote)
+            }.padding(.horizontal)
+            
+            
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Login")
+                    .padding()
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .frame(width: 360, height: 48)
+                    .foregroundStyle(.white)
+                    .background(.red)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                
+                
+                
+            })
+                
+            }.navigationTitle("Whislists")
+            
+            Spacer()
+            
+        }
     }
 }
 
