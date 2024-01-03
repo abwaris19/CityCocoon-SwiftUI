c0e927687bc5a6019c28368e0a1ba5ccc55d2c2c
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-03 05:54:44 +0100

    UI Wrap Up

---
+++ CityCocoon/Core/Wishlists/WishlistsView.swift
@@ -0,0 +1,18 @@
+//
+//  WishlistsView.swift
+//  CityCocoon
+//
+//  Created by Khan on 03.01.2024.
+//
+
+import SwiftUI
+
+struct WishlistsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WishlistsView()
+}
