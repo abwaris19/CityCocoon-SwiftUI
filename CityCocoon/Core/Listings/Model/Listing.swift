73aaa90fa6878ae46f742fe9f10145ed4d715030
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-03 21:08:02 +0100

    model

--- CityCocoon/Core/Listings/Model/Listing.swift
+++ CityCocoon/Core/Listings/Model/Listing.swift
@@ -6,3 +6,105 @@
 //
 
 import Foundation
+
+struct Listing: Identifiable, Codable {
+    var id = NSUUID().uuidString
+    let ownerUid: String
+    let ownerName: String
+    let ownerImageUrl: String
+    let numberOfBedrooms: Int
+    let numberOfBathrooms: Int
+    let numberOfGuests: Int
+    var pricePerNight: Int
+    let numberOfBeds: Int
+    var latitude: Double
+    var longitude: Double
+    var imageURLs: [String]
+    let address: String
+    let city: String
+    let state: String
+    let features: [ListingFeatures]
+    let amenities: [ListingAmenities]
+    let type: ListingType
+    let title: String
+}
+
+enum ListingFeatures: Int, Codable, Identifiable, Hashable {
+    case selfCheckIn
+    case superHost
+    
+    
+    var imageName: String {
+        switch self {
+        case .selfCheckIn: return "door.left.hand.open"
+        case .superHost: return "medal"
+        }
+    }
+        
+    var title: String {
+        switch self {
+        case .selfCheckIn: return "Self check-in"
+        case .superHost: return "Superhost"
+        }
+    }
+    
+    var subtitle: String {
+        switch self {
+        case .selfCheckIn:
+            return "Check yourself in with the keypad."
+        case .superHost:
+            return "Superhosts are experienced, highly rated hosts who are commited to providing greate stars for guests."
+        }
+    }
+    
+    var id: Int { return self.rawValue}
+}
+
+enum ListingAmenities: Int, Codable, Identifiable, Hashable {
+    case pool
+    case kitchen
+    case wifi
+    case laundry
+    case tv
+    case alarmSystem
+    case office
+    case balcony
+
+    
+    var title: String {
+        switch self {
+        case .pool: return "Pool"
+        case .kitchen: return "Kitchen"
+        case .wifi: return "Wifi"
+        case .laundry: return "Laundry"
+        case .tv: return "TV"
+        case .alarmSystem: return "Alarm System"
+        case .office: return "Office"
+        case .balcony: return "Balcony"
+        }
+    }
+    
+    
+    
+    var id: Int { return self.rawValue }
+}
+
+
+enum ListingType: Int, Codable, Identifiable, Hashable {
+    case apartment
+    case house
+    case townHouse
+    case villa
+    
+    var description: String {
+        switch self {
+        case .apartment: return "Apartment"
+        case .house: return "House"
+        case .townHouse: return "Town Home"
+        case .villa: return "Villa"
+        }
+    }
+    
+    var id: Int { return self.rawValue }
+}
+
--- CityCocoon/Core/ToolBar/MainTabView.swift
+++ CityCocoon/Core/ToolBar/MainTabView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct MainTabView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        TabView {
+            ExploreView()
+                .tabItem {  Label("Explore", systemImage: "magnifyingglass") }
+            WishlistsView()
+                .
+            tabItem {  Label("Explore", systemImage: "heart") }
+            ProfileView()
+                .
+            tabItem {  Label("Explore", systemImage: "person") }
+        }
     }
 }
 
