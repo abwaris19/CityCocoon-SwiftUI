05392410674e398f6afe5cb248ae15ded405e953
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-08 19:35:24 +0100

    Listing Map Preview View & Listing Detail View Presentation

---
+++ CityCocoon/Core/Listings/View/ListingMapPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingMapPreviewView.swift
+//  CityCocoon
+//
+//  Created by Khan on 08.01.2024.
+//
+
+import SwiftUI
+
+struct ListingMapPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingMapPreviewView()
+}
