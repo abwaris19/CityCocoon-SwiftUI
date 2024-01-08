38e05aabee288d6928cdf5f8817b8f785f070e2a
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-08 19:43:31 +0100

    Listing Map Preview View & Listing Detail View Presentation

--- CityCocoon/Core/Listings/View/ListingMapPreviewView.swift
+++ CityCocoon/Core/Listings/View/ListingMapPreviewView.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct ListingMapPreviewView: View {
+    let listing: Listing
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        VStack {
+            TabView {
+                ForEach(listing.imageURLs, id: \.self) { imageUrl in
+                    
+                    Image(imageUrl)
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                    
+                }
+            }
+            .frame(height: 270)
+            .tabViewStyle(.page)
+            
+            HStack (alignment: .top) {
+                
+                VStack(alignment: .leading) {
+                    Text("\(listing.city), \(listing.state)")
+                        .fontWeight(.semibold)
+                    
+                    Text("8 nights: Nov 12 -22")
+                    
+                    HStack {
+                        
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        
+                        Text("Night")
+                    }
+                }
+                
+                Spacer()
+                
+                Text("4.899")
+            }
+        }
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
     }
 }
 
 #Preview {
-    ListingMapPreviewView()
+    ListingMapPreviewView(listing: DeveloperPreview.shared.listings[0])
 }
