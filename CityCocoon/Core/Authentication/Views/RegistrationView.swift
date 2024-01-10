474fdb87b64b852a5c79b38443ef4b7b8e2cb666
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-10 11:34:55 +0100

    Register View

--- CityCocoon/Core/Authentication/Views/RegistrationView.swift
+++ CityCocoon/Core/Authentication/Views/RegistrationView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct RegistrationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+     
+        VStack {
+      
+            Image(.airbnbAppIcon)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 120, height: 120)
+                .padding()
+          
+        }
     }
 }
 
