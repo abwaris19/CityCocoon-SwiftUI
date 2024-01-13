6bf58b8150e94c789991c9670f477e281db45029
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-14 00:14:10 +0100

    User Profile Header

---
+++ CityCocoon/Core/Component/CircularProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularProfileView.swift
+//  CityCocoon
+//
+//  Created by Khan on 13.01.2024.
+//
+
+import SwiftUI
+
+struct CircularProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularProfileView()
+}
---
+++ CityCocoon/Core/Profile/UserProfileHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileHeaderView.swift
+//  CityCocoon
+//
+//  Created by Khan on 13.01.2024.
+//
+
+import SwiftUI
+
+struct UserProfileHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileHeaderView()
+}
